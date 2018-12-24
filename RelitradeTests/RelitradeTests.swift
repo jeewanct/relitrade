b6cf5c963d56793217730667960e96943c780288
Vaibhav Nath <macrochips@Vaibhavs-Mac-mini.local>
2018-12-24 16:19:58 +0530

    Initial Commit

---
+++ RelitradeTests/RelitradeTests.swift
@@ -0,0 +1,34 @@
+//
+//  RelitradeTests.swift
+//  RelitradeTests
+//
+//  Created by Vaibhav Nath on 24/12/18.
+//  Copyright © 2018 investwell. All rights reserved.
+//
+
+import XCTest
+@testable import Relitrade
+
+class RelitradeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
